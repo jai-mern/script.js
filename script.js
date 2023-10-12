570478830007eb9c3aa246d419cb773451c636ab
Jayakanth J <145441150+jai-mern@users.noreply.github.com>
2023-10-12 19:28:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function foo(){
+    let result=document.getElementById("email").Value;
+    //console.log(result);
+    if(result=="b52wdtamil@gmail.com"){
+        console.log("valid");
+    }
+    else
+    {
+        console.log("invalid");
+    }
+}
